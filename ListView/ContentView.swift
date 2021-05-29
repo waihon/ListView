5eeb2ec0f4c6f0d484bf54d41d55f36e8dc795b2
Waihon Yew <yewwaihon@gmail.com>
2021-05-29 13:00:34 +0800

    List can generate its rows entirely from dynamic content without needing a ForEach

--- ListView/ContentView.swift
+++ ListView/ContentView.swift
@@ -9,24 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    List {
-      Section(header: Text("Section 1")) {
-        Text("Static row 1")
-        Text("Static row 2")
-      }
-
-      Section(header: Text("Section 2")) {
-        ForEach(0 ..< 5) {
-          Text("Dynamic row \($0)")
-        }
-      }
-      
-      Section(header: Text("Section 3")) {
-        Text("Static row 3")
-        Text("Static row 4")
-      }
+    List(0 ..< 5) {
+      Text("List dynamic row \($0)")
     }
-    .listStyle(GroupedListStyle())
   }
 }
 
